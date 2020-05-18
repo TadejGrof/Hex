f71e584724e68389112817f8118fda82399768ef
nkovacic99 <nace.kovacic007@gmail.com>
2020-05-18 18:40:10 +0200

    popravil problem, kjer je pobarvalo vse blocke naenkrat

--- src/koordinati/Koordinati.java
+++ src/koordinati/Koordinati.java
@@ -1,19 +1,19 @@
 package koordinati;
 
 public class Koordinati {
-	private static int x;
-	private static int y;
+	private int x;
+	private int y;
 	
 	public Koordinati(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	public static int getX() { 
+	public int getX() { 
 		return x; 
 	}
 
-	public static int getY() {
+	public int getY() {
 		return y;
 	}
 
